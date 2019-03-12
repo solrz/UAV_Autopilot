bb9274f78d34d578670c0bc853ffc1bf16b2ca2b
hackintosh <14904805+solrz@users.noreply.github.com>
2019-03-12 14:39:33 +0800

    Lab1 Done: convert picture to Grayscale

--- LAB1/main.py
+++ LAB1/main.py
@@ -2,6 +2,20 @@ import cv2
 import time
 
 mat = cv2.imread("/Users/hackintosh/Desktop/IMG_0086.jpg")
+RESET = 0
+width, height = mat.shape[:2]
+for row in range(height):
+    for col in range(width):
+        sum_pixel = sum(mat[row,col]) / 3
+        '''
+        print(sum_pixel)
+        print(pixel)
+        print(sum(pixel))
+        '''
+        mat[row,col] = (sum_pixel,sum_pixel,sum_pixel)
+
+
+print(mat[0][0])
 window = cv2.namedWindow("wow",0)
 cv2.imshow("wow", mat)
 cv2.waitKey(0)
